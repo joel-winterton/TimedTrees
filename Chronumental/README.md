9a97de61914fd31b962c6ac0b6abda51dcd87ba0
Joel Winterton <joel.winterton@outlook.com>
2024-09-05 18:21:06 +0100

    Command line interface

--- Chronumental/README.md
+++ Chronumental/README.md
@@ -9,3 +9,9 @@ Then prepare the SuperSimPy output directory using:
 ```shell
 python3 Chronumental/prepare_sim.py --datapath=<absolute path to SuperSimPy output dir>
 ```
+Chronumental can then be run on the simulation: 
+
+```shell
+cd <path to SuperSimPy output dir>/Chronumental
+chronumental --tree input.tree --dates dates.csv --name_all_nodes --only_use_full_dates
+```
--- benchmark.py
+++ benchmark.py
@@ -1,76 +1,27 @@
-"""
-ENTRYPOINT.
-This script accepts dataset paths and outputs benchmarks.
-Currently implemented:
-1. TreeTime
-"""
-import matplotlib.pyplot as plt
-from parse_dates import input_dates
-from compare_dataframes import compare_dataframes
+import argparse
+from graphs import Graphs
 
-import matplotlib as mpl
+parser = argparse.ArgumentParser(
+    prog='TimeTreeBenchmarker',
+    description="Command line interface to run benchmarks on time tree estimation algorithm outputs.")
+parser.add_argument('-o', '--output_dir', required=True)
+parser.add_argument('-d', '--dates_true', required=True, help='Path to dated metadata TSV.')
+parser.add_argument('-t', '--tree_true', required=True,
+                    help='Path to substitutions per site scaled phylogeny in Newick format.')
+parser.add_argument('-d1', '--dates_treetime', required=False, help='Path to dates TSV outputted from TreeTime.')
+parser.add_argument('-d2', '--dates_chronumental', required=False,
+                    help='Path to dates TSV outputted from Chronumental.')
 
-mpl.rcParams['figure.dpi'] = 600
+args = vars(parser.parse_args())
+graph_args = dict()
+graph_args['output_dir'] = args['output_dir'] if args['output_dir'][-1] == '/' else args['output_dir'] + '/'
+graph_args['true_dates_path'] = args['dates_true']
+graph_args['source_tree_path'] = args['tree_true']
 
-
-class Benchmark:
-    def __init__(self, true_dates_path: str, source_tree_path: str, treetime_date_path=None,
-                 chronumental_date_path=None):
-        self.truth = input_dates('truth', datepath=true_dates_path)
-        self.algorithms = dict()
-        if treetime_date_path:
-            self.algorithms['TreeTime'] = input_dates('treetime', datepath=treetime_date_path,
-                                                      treepath=source_tree_path)
-        if chronumental_date_path:
-            self.algorithms['Chronumental'] = input_dates('chronumental', datepath=chronumental_date_path,
-                                                          treepath=source_tree_path)
-
-    def run(self, algorithm: str):
-        if algorithm in self.algorithms:
-            return self.graph(self.truth, self.algorithms[algorithm], algorithm)
-        else:
-            raise ValueError('Algorithm not recognized!')
-
-    def graph(self, truth, estimated, algorithm):
-        comparison = compare_dataframes(truth, estimated)
-        number_of_samples = (estimated.shape[0] - 1) / 2
-        fig, axs = plt.subplots(2, figsize=(6, 8))
-        ax1, ax2 = axs
-        fig.suptitle(f'{algorithm} benchmark with {str(number_of_samples)} samples.')
-
-        # Separate into internal and external nodes
-        external = comparison[comparison['internal'] == False]
-        internal = comparison[comparison['internal'] == True]
-
-        # Plot true dates
-        ax1.scatter(external['id'], external['date_true'], c='green', alpha=0.5, s=3, marker='^',
-                    label='True external date')
-        ax1.scatter(external['id'], external['date_estimated'], c='red', marker='^', s=1,
-                    label='Estimated external date')
-        ax1.scatter(internal['id'], internal['date_estimated'], c='red', s=3,
-                    label='Estimated internal date')
-
-        ax1.scatter(internal['id'], internal['date_true'], c='green', s=1, label='True internal date')
-
-        ax1.legend()
-        ax1.set_title('Date error per node')
-        ax1.set_xlabel('Node ID (post-order)')
-        ax1.set_ylabel('Date')
-
-        ax2.scatter(comparison['date_true'], comparison['difference'], c='red', s=0.5)
-        ax2.set_ylabel('Error (days)')
-        ax2.set_xlabel('Node true date')
-        ax2.set_ylim([-300, 800])
-        for q in internal['difference'].quantile([0.25, 0.5, 0.75]):
-            print(q)
-        plt.show(block=True)
-
-
-if __name__ == '__main__':
-    benchmarker = Benchmark(true_dates_path='/home/joel/EBI/TimedTreesData/1000/dated_country_labelled_metadata.tsv',
-                            source_tree_path='/home/joel/EBI/TimedTreesData/1000/sim.substitutions.tree',
-                            treetime_date_path='/home/joel/EBI/TimedTreesData/1000/TreeTime/dates.tsv',
-                            chronumental_date_path='/home/joel/EBI/TimedTreesData/1000/Chronumental'
-                                                   '/chronumental_dates.tsv')
-    benchmarker.run('Chronumental')
-    benchmarker.run('TreeTime')
+if 'dates_treetime' in args:
+    graph_args['treetime_date_path'] = args['dates_treetime']
+if 'dates_chronumental' in args:
+    graph_args['chronumental_date_path'] = args['dates_chronumental']
+print("Creating graphs!")
+grapher = Graphs(*graph_args)
+grapher.run_all()
